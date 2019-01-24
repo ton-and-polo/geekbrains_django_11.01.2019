7b4d7471d32dc76a99891da0f6ad7d764f51c3db
ton-and-polo <ton.and.polo@gmail.com>
2019-01-25 00:41:25 +0300

    Add catigory to the poduct

--- products/models.py
+++ products/models.py
@@ -4,6 +4,13 @@ from django.urls import reverse
 # Create your models here.
 
 
+class Category(models.Model):
+    category_name = models.CharField(max_length=50, null=True)
+
+    def __str__(self):
+        return self.category_name
+
+
 class Product(models.Model):
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10000, decimal_places=2)
@@ -12,6 +19,7 @@ class Product(models.Model):
         null=True
     )
     description = models.TextField(blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def get_absolute_url(self):
         return reverse("products:product-details", kwargs={"id": self.id})
