d8981d681dccf5c258659f03338ad47862b2435e
ton-and-polo <ton.and.polo@gmail.com>
2019-01-31 22:38:12 +0300

    Apend cart items counter

--- products/urls.py
+++ products/urls.py
@@ -7,6 +7,6 @@ from .views import (
 
 app_name = 'products'
 urlpatterns = [
-    path('', products_list_view, name='products-list'),
+    path('<str:category_name>', products_list_view, name='products-list'),
     path('<int:id>/', product_details_view, name='product-details')
 ]
--- products/views.py
+++ products/views.py
@@ -6,17 +6,23 @@ from shopping_cart.models import ShoppingCart
 # Create your views here.
 
 
-def products_list_view(request):
+def products_list_view(request, category_name):
     categories = Category.objects.all()
-    user_cart = ShoppingCart.objects.filter(id=request.user.id)
-    products = Product.objects.all()
+    user_cart = ShoppingCart.objects.filter(user=request.user.id)
+
+    if category_name == 'all':
+        products = Product.objects.all()
+    else:
+        category = Category.objects.filter(category_name=category_name)[0]
+        products = Product.objects.filter(category=category)
 
 
 
     context = {
         "products": products,
         "categories": categories,
-        "user_cart": user_cart
+        "user_cart": user_cart,
+        "all_products": "all"
     }
     return render(request, "products/products_list.html", context)
 
